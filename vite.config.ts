b60e2952b043211a916c9944de2940e7c481bde8
Darcy Liu <d273liu@uwaterloo.ca>
2025-06-30 00:19:06 -0400

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
+
+const conditionalPlugins: [string, Record<string, any>][] = [];
+
+// @ts-ignore
+if (process.env.TEMPO === "true") {
+  conditionalPlugins.push(["tempo-devtools/swc", {}]);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +17,9 @@ export default defineConfig({
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
   plugins: [
-    react(),
+    react({
+      plugins: conditionalPlugins,
+    }),
     tempo(),
   ],
   resolve: {
